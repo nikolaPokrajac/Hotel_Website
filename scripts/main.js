613abc1b1b34d8ae84dd69512d21177d86984c3f
Nikola Pokrajac <nikolapokrajac92@gmail.com>
2018-10-31 13:16:56 +0100

    Smooth scrolling & animations.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    /* Sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -10,4 +11,33 @@ $(document).ready(function () {
         offset: '60px;'
     });
 
+    /* Scroll on buttons */
+    $('.js--scroll-to-rooms').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-rooms').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html,body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+    /* Scroll on nav links */
+    var scrollLink = $('.scroll');
+
+    scrollLink.click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+    /* Animations on scroll*/
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
 });
